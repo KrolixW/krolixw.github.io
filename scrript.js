ed4113ba4ce6c52192956603c1e8de08c0361fea
KrolixW <krolikwormix@gmail.com>
2022-04-17 00:04:48 +0300

    some garbage

--- scrript.js
+++ scrript.js
@@ -1,3 +1,6 @@
 <script src="https://telegram.org/js/telegram-web-app.js"></script>
+let data;
+document.getElementById("demo").innerHTML = "Date : " + Date();
+data = window.Telegram.WebApp.initData;
+alert(data);
 
-document.getElementById("demo").innerHTML = "Date : " + window.Telegram.WebApp.initData;
