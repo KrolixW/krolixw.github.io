ce8f866e4ba7679f8625d0596210c4c21d3457e3
KrolixW <krolikwormix@gmail.com>
2022-04-17 00:35:41 +0300

    hmm

--- scrript.js
+++ scrript.js
@@ -1,8 +1,9 @@
 
 let data;
-document.getElementById("demo").innerHTML = "Date : " + Date();
+document.getElementById("demo").innerHTML = "Date : ";
 data = window.Telegram.WebApp.initData;
 document.getElementById("test").innerHTML(data);
 window.Telegram.WebApp.ready();
+alert("cock")
 // alert(data);
 
