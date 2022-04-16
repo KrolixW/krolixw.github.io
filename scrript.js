7b332573d31dfb8c7a3d5a1f405488ec2a3a6f01
KrolixW <krolikwormix@gmail.com>
2022-04-17 00:12:17 +0300

    removed for test

--- scrript.js
+++ scrript.js
@@ -1,6 +1,6 @@
 <script src="https://telegram.org/js/telegram-web-app.js"></script>
-let data;
+//let data;
 document.getElementById("demo").innerHTML = "Date : " + Date();
-data = window.Telegram.WebApp.initData;
-alert(data);
+//data = window.Telegram.WebApp.initData;
+// alert(data);
 
