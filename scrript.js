2ddec436a755a1a3475df0227f22bf9760a88b52
KrolixW <krolikwormix@gmail.com>
2022-04-17 00:27:09 +0300

    wtf

--- scrript.js
+++ scrript.js
@@ -1,4 +1,4 @@
-<script src="https://telegram.org/js/telegram-web-app.js"></script>
+
 let data;
 document.getElementById("demo").innerHTML = "Date : " + Date();
 data = window.Telegram.WebApp.initData;
