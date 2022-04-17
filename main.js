771a371972ca6efb90799482394fd18383dc116a
KrolixW <krolikwormix@gmail.com>
2022-04-17 16:10:03 +0300

    added button and click counter

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 // JavaScript
 let firstname = window.Telegram.WebApp.initDataUnsafe.user.first_name;
 let lastname = window.Telegram.WebApp.initDataUnsafe.user.last_name;
+let clicks = 0;
+function add_click(){
+    clicks++;
+    document.getElementById('button_output').innerHTML = "Button was clicked "+clicks+" time(s)";
+}
 document.getElementById('welcome').innerText += " "+firstname +" "+lastname;
+document.getElementById('my_butt').onclick(add_click);
