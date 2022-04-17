01fc6ea5934245dc55cab9c6ad54746e5434282b
KrolixW <krolikwormix@gmail.com>
2022-04-17 15:45:49 +0300

    aaaaaaaaaaaaaaAфaAAAA

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // JavaScript
 let firstname = window.Telegram.WebApp.initDataUnsafe.user.first_name;
 let lastname = window.Telegram.WebApp.initDataUnsafe.user.last_name;
-document.getElementById('welcome').innerText += firstname;
-document.getElementById('welcome').innerText += lastname;
+document.getElementById('welcome').innerText += " "+firstname +" "+lastname;
