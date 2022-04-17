2f32ab3cfed372faf851953064119480c69aeb51
KrolixW <krolikwormix@gmail.com>
2022-04-17 16:14:24 +0300

    function fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let lastname = window.Telegram.WebApp.initDataUnsafe.user.last_name;
 let clicks = 0;
 function add_click(){
     clicks++;
-    document.getElementById('button_output').innerHTML = "Button was clicked "+clicks+" time(s)";
+    document.getElementById('button_output').innerText = "Button was clicked "+clicks+" time(s)";
 }
 document.getElementById('welcome').innerText += " "+firstname +" "+lastname;
 document.getElementById('my_butt').onclick(add_click);
