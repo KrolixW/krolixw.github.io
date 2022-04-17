6ec9167e67da7be346ad3263f2f5cce2a9104541
KrolixW <krolikwormix@gmail.com>
2022-04-17 23:24:23 +0300

    less

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createRipple(event) {
     const circle = document.createElement("span");
     const diameter = Math.max(button.clientWidth, button.clientHeight);
     const radius = diameter / 2;
-    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.width = circle.style.height = `${diameter-10}px`;
     circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
     circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
     circle.classList.add("ripple");
